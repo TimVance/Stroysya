9678624ef42d8e0573cae64af7e95e1c38b6f151
TimVance <2DngxvshA6GUGcQ>
2020-08-21 19:20:24 +0300

    Вывел каталог услуг

---
+++ bitrix/templates/aspro_next/components/bitrix/news/uslugi/news.php
@@ -0,0 +1,78 @@
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+/** @var array $arParams */
+/** @var array $arResult */
+/** @global CMain $APPLICATION */
+/** @global CUser $USER */
+/** @global CDatabase $DB */
+/** @var CBitrixComponentTemplate $this */
+/** @var string $templateName */
+/** @var string $templateFile */
+/** @var string $templateFolder */
+/** @var string $componentPath */
+/** @var CBitrixComponent $component */
+$this->setFrameMode(true);
+?>
+
+<?if($arParams["USE_RSS"]=="Y"):?>
+	<?
+	if(method_exists($APPLICATION, 'addheadstring'))
+		$APPLICATION->AddHeadString('<link rel="alternate" type="application/rss+xml" title="'.$arResult["FOLDER"].$arResult["URL_TEMPLATES"]["rss"].'" href="'.$arResult["FOLDER"].$arResult["URL_TEMPLATES"]["rss"].'" />');
+	?>
+	<a href="<?=$arResult["FOLDER"].$arResult["URL_TEMPLATES"]["rss"]?>" title="rss" target="_self"><img alt="RSS" src="<?=$templateFolder?>/images/gif-light/feed-icon-16x16.gif" border="0" align="right" /></a>
+<?endif?>
+
+<?if($arParams["USE_SEARCH"]=="Y"):?>
+<?=GetMessage("SEARCH_LABEL")?><?$APPLICATION->IncludeComponent(
+	"bitrix:search.form",
+	"flat",
+	Array(
+		"PAGE" => $arResult["FOLDER"].$arResult["URL_TEMPLATES"]["search"]
+	),
+	$component
+);?>
+<br />
+<?endif?>
+<?if($arParams["USE_FILTER"]=="Y"):?>
+<?$APPLICATION->IncludeComponent(
+	"bitrix:catalog.filter",
+	"",
+	Array(
+		"IBLOCK_TYPE" => $arParams["IBLOCK_TYPE"],
+		"IBLOCK_ID" => $arParams["IBLOCK_ID"],
+		"FILTER_NAME" => $arParams["FILTER_NAME"],
+		"FIELD_CODE" => $arParams["FILTER_FIELD_CODE"],
+		"PROPERTY_CODE" => $arParams["FILTER_PROPERTY_CODE"],
+		"CACHE_TYPE" => $arParams["CACHE_TYPE"],
+		"CACHE_TIME" => $arParams["CACHE_TIME"],
+		"CACHE_GROUPS" => $arParams["CACHE_GROUPS"],
+		"PAGER_PARAMS_NAME" => $arParams["PAGER_PARAMS_NAME"],
+	),
+	$component
+);
+?>
+<br />
+<?endif?>
+<?$APPLICATION->IncludeComponent(
+    "bitrix:catalog.section.list",
+    "",
+    Array(
+        "ADD_SECTIONS_CHAIN" => "Y",
+        "CACHE_FILTER" => "N",
+        "CACHE_GROUPS" => "Y",
+        "CACHE_TIME" => "36000000",
+        "CACHE_TYPE" => "A",
+        "COUNT_ELEMENTS" => "Y",
+        "COUNT_ELEMENTS_FILTER" => "CNT_ACTIVE",
+        "FILTER_NAME" => "arrFilter",
+        "IBLOCK_ID" => "37",
+        "IBLOCK_TYPE" => "services",
+        "SECTION_CODE" => $_REQUEST["SECTION_CODE"],
+        "SECTION_FIELDS" => array("", ""),
+        "SECTION_ID" => $_REQUEST["SECTION_ID"],
+        "SECTION_URL" => "",
+        "SECTION_USER_FIELDS" => array("", ""),
+        "SHOW_PARENT_NAME" => "Y",
+        "TOP_DEPTH" => "2",
+        "VIEW_MODE" => "LINE"
+    )
+);?>
